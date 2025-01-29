6a7ebf5460a13fc6ad2e61eb24abeb99ab91bdec
Christopher Uy <123910681+chuy110@users.noreply.github.com>
2025-01-29 03:57:59 -0800

    Added temporary placeholder card class

--- Cpsc370Final/Deck.cs
+++ Cpsc370Final/Deck.cs
@@ -1,5 +1,24 @@
+using System;
+using System.Collections.Generic;
 namespace Cpsc370Final;
 
+// Added temporary placeholder card class
+public class Card
+{
+    public string Suit { get; private set; }
+    public string Rank { get; private set; }
+
+    public Card(string suit, string rank)
+    {
+        Suit = suit;
+        Rank = rank;
+    }
+
+    public override string ToString()
+    {
+        return $"{Rank} of {Suit}";
+    }
+}
 public class Deck
 {
     
