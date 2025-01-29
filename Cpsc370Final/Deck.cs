17f32569a4dcd1694d07d96a8972829d9dbe8bf3
Christopher Uy <123910681+chuy110@users.noreply.github.com>
2025-01-29 04:00:32 -0800

    Updated Deck class Added Reset/Shuffle method Added DrawCard method

--- Cpsc370Final/Deck.cs
+++ Cpsc370Final/Deck.cs
@@ -21,5 +21,54 @@ public class Card
 }
 public class Deck
 {
-    
+    private List<Card> cards;
+    private Random random = new Random();
+
+    public Deck()
+    {
+        ResetAndShuffle();
+    }
+
+    public void ResetAndShuffle()
+    {
+        cards = new List<Card>();
+        string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+        string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+        foreach (string suit in suits)
+        {
+            foreach (string rank in ranks)
+            {
+                cards.Add(new Card(suit, rank));
+            }
+        }
+
+        Shuffle();
+    }
+
+    private void Shuffle()
+    {
+        // Fisher-Yates Shuffle
+        for (int n = cards.Count - 1; n > 0; --n)
+        {
+            int k = random.Next(n + 1);
+            Card temp = cards[n];
+            cards[n] = cards[k];
+            cards[k] = temp;
+        }
+    }
+
+    public Card DrawCard()
+    {
+        if (cards.Count > 0)
+        {
+            Card cardToDraw = cards[0];
+            cards.RemoveAt(0);
+            return cardToDraw;
+        }
+        else
+        {
+            throw new InvalidOperationException("No cards left in the deck.");
+        }
+    }
 }
