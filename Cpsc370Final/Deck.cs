49155d1727973b233767ef16ae122d496d28cfc4
Brendan Chen <me@bchen.dev>
2025-01-29 19:33:13 -0800

    remove duplicate Card class

--- Cpsc370Final/Deck.cs
+++ Cpsc370Final/Deck.cs
@@ -3,22 +3,6 @@ using System.Collections.Generic;
 namespace Cpsc370Final;
 
 // Added temporary placeholder card class
-public class Card
-{
-    public string Suit { get; private set; }
-    public string Rank { get; private set; }
-
-    public Card(string suit, string rank)
-    {
-        Suit = suit;
-        Rank = rank;
-    }
-
-    public override string ToString()
-    {
-        return $"{Rank} of {Suit}";
-    }
-}
 public class Deck
 {
     private List<Card> cards;
