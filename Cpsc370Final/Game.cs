7235d12426b0317dc3a74f8303ad88e3543795b9
Christopher Uy <123910681+chuy110@users.noreply.github.com>
2025-01-29 22:44:57 -0800

    Added NewRound and DrawCard methods

--- Cpsc370Final/Game.cs
+++ Cpsc370Final/Game.cs
@@ -6,11 +6,41 @@ namespace Cpsc370Final
     public class Game
     {
         public Deck Deck { get; private set; }
-        public List<Card> Playerhan { get; private set; }
+        public List<Card> PlayerHand { get; private set; }
         public int PlayerScore { get; private set; }
         public bool GameOver { get; private set; }
 
         public Game()
+        {
+            Deck = new Deck();
+            PlayerHand = new List<Card>();
+            GameOver = false;
+            NewRound();
+        }
+
+        public void NewRound()
+        {
+            Deck.ResetAndShuffle();
+            PlayerHand.Clear();
+            PlayerScore = 0;
+            GameOver = false;
+            DrawCard();
+            DrawCard();
+        }
+
+        public void DrawCard()
+        {
+            Card card = Deck.DrawCard();
+            PlayerHand.Add(card);
+            PlayerScore += card.Value;
+            CheckAceAdjustment();
+            if (PlayerScore > 21)
+            {
+                GameOver = true;
+            }
+        }
+
+        public void CheckAceAdjustment()
         {
             
         }
