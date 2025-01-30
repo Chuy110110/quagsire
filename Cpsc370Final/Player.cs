230064d6b6c126a8dd5ecedc6e71d0f81c53f77f
Brendan Chen <me@bchen.dev>
2025-01-29 20:42:40 -0800

    add Player properties and method stubs

--- Cpsc370Final/Player.cs
+++ Cpsc370Final/Player.cs
@@ -2,5 +2,29 @@ namespace Cpsc370Final;
 
 public class Player
 {
-    
+    private const int StartingBalance = 1000;
+
+    private int balance;
+    public List<Card> hand { get; private set; }
+    private int score;
+
+    private bool _isStandingForCurrentRound = false;
+
+    public Player()
+    {
+        balance = StartingBalance;
+        hand = new List<Card>();
+    }
+
+    public void AddCardToHand(Card card)
+    {
+    }
+
+    public void ClearHand()
+    {
+    }
+
+    public void MarkAsStandingForCurrentRound()
+    {
+    }
 }
