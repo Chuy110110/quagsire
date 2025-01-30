6abff6aa1a50908e02aeeb288d66eef717d38591
Brendan Chen <me@bchen.dev>
2025-01-29 16:04:03 -0800

    add deck test cases

--- Cpsc370Final.Tests/Deck_tests.cs
+++ Cpsc370Final.Tests/Deck_tests.cs
@@ -2,5 +2,19 @@ namespace Cpsc370Final.Tests;
 
 public class Deck_tests
 {
-    
+    [Fact]
+    public void ResetAndShuffleConstructsRandomizedCardList()
+    {
+    }
+
+    [Fact]
+    public void DrawCardDrawsFirstCardFromDeck()
+    {
+
+    }
+
+    [Fact]
+    public void DrawCardThrowsCorrectExceptionAfterDrawingAllCards()
+    {
+    }
 }
