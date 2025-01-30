8d2b86abda459cef2be6a8a388e21522fc132f79
Christopher Uy <123910681+chuy110@users.noreply.github.com>
2025-01-29 20:39:39 -0800

    Added basic variables to Game class

--- Cpsc370Final/Game.cs
+++ Cpsc370Final/Game.cs
@@ -1,6 +1,19 @@
-namespace Cpsc370Final;
+using System;
+using System.Collections.Generic;
 
-public class Game
+namespace Cpsc370Final
 {
-    
+    public class Game
+    {
+        public Deck Deck { get; private set; }
+        public List<Card> Playerhan { get; private set; }
+        public int PlayerScore { get; private set; }
+        public bool GameOver { get; private set; }
+
+        public Game()
+        {
+            
+        }
+    }
+
 }
