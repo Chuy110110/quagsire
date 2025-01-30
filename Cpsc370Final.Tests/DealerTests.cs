32f38e9fb830b9ed3c5311f76c9a644e90d93bb9
Tim Deutschman <timdeu05@gmail.com>
2025-01-29 16:56:29 -0800

    added face up and face down for dealer with tests

--- Cpsc370Final.Tests/DealerTests.cs
+++ Cpsc370Final.Tests/DealerTests.cs
@@ -13,8 +13,9 @@ public class DealerTests
         
         Assert.Equal(expected, result);
     }
-
+    
     [Fact]
+    
     public void Dealer_choosesAction_usingHitOrStand()
     {
         var sut = new Dealer();
@@ -32,5 +33,27 @@ public class DealerTests
         var result = shuffleDeck();
         
         
+    }
+    
+
+    [Theory]
+    [InlineData("2", "7", false)]
+    [InlineData("2", "8", true)]
+    public void Dealer_faceupandfacedown(string card1, string card2, bool turn)
+    {
+        var sut = new Dealer();
+        
+        var result = sut.FaceupFacedown(card1, card2, turn);
+        if (turn == false)
+        {
+            Assert.Equal(card1, result.Item1);
+            Assert.Equal("??", result.Item2);
+        }
+
+        if (turn == true)
+        {
+            Assert.Equal(card1, result.Item1);
+            Assert.Equal(card2, result.Item2);
+        }
     }
 }
--- Cpsc370Final/Dealer.cs
+++ Cpsc370Final/Dealer.cs
@@ -16,4 +16,20 @@ public class Dealer
         }
         return 0;
     }
+
+    
+    public (string, string) FaceupFacedown(string card1, string card2, bool dealer_turn = false)
+    {
+        string dealer_card1 = card1;
+        string dealer_card2 = card2;
+        string facedown = "??";
+        if (dealer_turn == false)
+        {
+            return (dealer_card1, facedown);
+        }
+        else
+        {
+            return (dealer_card1, dealer_card2);
+        }
+    }
 }
