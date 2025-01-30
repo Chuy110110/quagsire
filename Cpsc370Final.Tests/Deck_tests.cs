689f64d46586b75f197e3fe347849fba08e587f4
Brendan Chen <me@bchen.dev>
2025-01-29 16:24:11 -0800

    add test to draw a single card from the deck

--- Cpsc370Final.Tests/Deck_tests.cs
+++ Cpsc370Final.Tests/Deck_tests.cs
@@ -10,7 +10,22 @@ public class Deck_tests
     [Fact]
     public void DrawCardDrawsFirstCardFromDeck()
     {
+        // Arrange
+        const int seed = 64;
 
+        var random = new Random(seed);
+        var cards = new List<Card>();
+
+        var deck = new Deck(random, cards);
+        deck.ResetAndShuffle();
+        // At this point, cards should have 52 cards
+
+        // Act
+        var expectedCard = cards[0];
+        var card = deck.DrawCard();
+
+        // Assert
+        Assert.Equal(expectedCard, card);
     }
 
     [Fact]
